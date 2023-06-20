c55cfc5efc76c9bd1af83f402c86ae817e288a0e
allan.vralmeida <allan.vralmeida@MAR0625640W10-1>
2023-06-20 14:10:23 -0300

    Adicionar arquivos de projeto.

---
+++ Teste/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double nota1, nota2, nota3, nota4, media;
+
+            Console.Write("Insira sua nota 1: ");
+            nota1= Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Insira sua nota 2: ");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Insira sua nota 3: ");
+            nota3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Insira sua nota 4: ");
+            nota4 = Convert.ToDouble(Console.ReadLine());
+
+            media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+            if (media >= 7) {
+                Console.WriteLine("Você foi aprovado !!");
+
+            }
+            else
+            {
+                Console.WriteLine("Você foi reprovado !!");
+            }
+        }
+    }
+}
